00283ee907cb20eb42aabe977cd51659e705e336
Ivan Vendrov <ivan@anthropic.com>
2023-02-13 14:53:45 -0800

    Add AnthropicInstruct to __init__

--- langchain/llms/__init__.py
+++ langchain/llms/__init__.py
@@ -2,7 +2,7 @@
 from typing import Dict, Type
 
 from langchain.llms.ai21 import AI21
-from langchain.llms.anthropic import Anthropic
+from langchain.llms.anthropic import Anthropic, AnthropicInstruct
 from langchain.llms.base import BaseLLM
 from langchain.llms.cohere import Cohere
 from langchain.llms.huggingface_hub import HuggingFaceHub
@@ -12,6 +12,7 @@ from langchain.llms.openai import AzureOpenAI, OpenAI
 
 __all__ = [
     "Anthropic",
+    "AnthropicInstruct",
     "Cohere",
     "NLPCloud",
     "OpenAI",
@@ -24,6 +25,7 @@ __all__ = [
 type_to_cls_dict: Dict[str, Type[BaseLLM]] = {
     "ai21": AI21,
     "anthropic": Anthropic,
+    "anthropic_instruct": AnthropicInstruct,
     "cohere": Cohere,
     "huggingface_hub": HuggingFaceHub,
     "nlpcloud": NLPCloud,
